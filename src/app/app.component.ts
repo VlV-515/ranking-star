b463d15c6d938e415ffaff21f7ef82ea3eeb42d7
VlV <92279938+VlV-515@users.noreply.github.com>
2022-03-03 01:04:11 -0600

    feat⚙️:Add Finish V1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
-
+interface DiccionaryInt {
+  item: number;
+  state: boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ranking-star';
+  diccionary: DiccionaryInt[] = [
+    {
+      item: 1,
+      state: false,
+    },
+    {
+      item: 2,
+      state: false,
+    },
+    {
+      item: 3,
+      state: false,
+    },
+    {
+      item: 4,
+      state: false,
+    },
+    {
+      item: 5,
+      state: false,
+    },
+  ];
+
+  onOver(num: number): void {
+    for (let index = 0; index <= num; index++) {
+      this.printElement(index);
+    }
+  }
+  onLeave(num: number): void {
+    for (let index = 0; index <= num; index++) {
+      this.cleanElement(index);
+    }
+  }
+  onClick(item: number) {
+    window.alert(`La puntuacion es ${item} estrellas. =)`);
+  }
+
+  printElement(num: number): void {
+    this.diccionary.find((elemento) => {
+      if (elemento.item == num) elemento.state = true;
+    });
+  }
+  cleanElement(num: number): void {
+    this.diccionary.find((elemento) => {
+      if (elemento.item == num) elemento.state = false;
+    });
+  }
 }
